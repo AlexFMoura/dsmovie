170495ab8a85323a9ca49037433337407ace1a15
alexfmoura <alexmoura2907@gmail.com>
2022-01-16 23:50:38 -0300

    Find movies

---
+++ backend/src/main/java/com/alex/dsmove/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.alex.dsmove.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alex.dsmove.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+}
